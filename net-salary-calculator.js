28752ff4f9fcefee87fe3c6bc4659234c8aa7a14
git_bnderitu <bnnderitu01@email.com>
2022-11-30 14:36:51 +0300

    Made changes to the net salary calculator project.

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -1,25 +1,45 @@
-const calculatePayeTax = (grossSalary) => {
-    const payeTax = grossSalary * 0.3;
-    return payeTax;
+
+const net = require("prompt-sync");
+//this makes the file to depend on the promt function
+const prompt = net();
+let salary = prompt("write your income:");
+//where the user inputs his or her income
+function taxRate(){
+    //fuction that calculates the tax rate
+    if(salary <= 24000){
+        return(salary/100)*10;
+    }
+    else if(salary > 24000 && salary <= 32333){
+        return(salary/100)*25;
+    }
+    else{
+        return(salary/100)*30;
+    }
 }
-
-const calculateNhif = (grossSalary) => {
-    const nhif = grossSalary * 0.1;
-    return nhif;
-};
-
-const calculateNssf = (grossSalary) => {
-    const nssf = grossSalary * 0.05;
-    return nssf;
-};
-
-const netSalaryCalculator = (grossSalary, benefits) => {
-   //Calculate the PAYE tax from KRA and NHIF
-    const payeTax = calculatePayeTax(grossSalary);
-    const nhif = calculateNhif(grossSalary);
-    const nssf = calculateNssf(grossSalary);
-    const totalDeductions = payeTax + nhif + nssf;
-    const netSalary = grossSalary - totalDeductions - benefits;
-    return netSalary;
-};
-
+let tax = taxRate();
+function medicalCover(){
+    //function that calculates medical cover
+    if (salary < 100000){
+        return salary*(1.6/100);
+    }
+    else{
+        return 1700
+    }
+}
+let NHIF = medicalCover();
+function nationalSecurity(){
+    //functions that calculates nssf deductions
+    if(salary <= 100000){
+        return salary*(6/100);
+    }
+    else{
+        return 6000;
+    }
+}
+let NSSF = nationalSecurity();
+console.log(`Gross Salary: Ksh.${salary}`);
+console.log(`Tax:Ksh.${tax}`);
+console.log(`NHIF Deductions: Ksh.${NHIF}`);
+console.log(`NSSF Deductions: Ksh.${NSSF}`);
+let netSalary = salary-(tax + NSSF + NHIF);
+console.log(`Net Salary: Ksh.${netSalary}`);
